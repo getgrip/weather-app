31b5880887f2bb241c5b04223f912d88f2daac1a
getgrip <tengis0621@gmail.com>
2019-11-14 22:43:23 -0500

    Updates

---
+++ precache-manifest.f6651d4b0fb72e9a7e487b81ca533c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442cef284ae9e6440ccf2a26840564a4",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "1f85a1fc8c8b6e6ebaf7",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4ff59fea85284f0e45cc",
+    "url": "/weather-app/static/css/main.9d044629.chunk.css"
+  },
+  {
+    "revision": "1f85a1fc8c8b6e6ebaf7",
+    "url": "/weather-app/static/js/2.2f103e12.chunk.js"
+  },
+  {
+    "revision": "4ff59fea85284f0e45cc",
+    "url": "/weather-app/static/js/main.3c419b54.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  }
+]);
